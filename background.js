87d77b1dc7f4aabfea6fa325c35dc0659c00a89b
Ian <nator419@yahoo.com>
2023-05-23 18:22:46 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,10 @@
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        console.log("blocking:", details.url);
+        return {cancel: true };
+    },
+    {urls: ["*://*.youtube.com/"]},
+    ["blocking"]
+);
